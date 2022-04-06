be8f832e3e9adc6f8d1268ba5e99b94905d9e7b8
unknown <pearsenation@gmail.com>
2022-04-06 17:05:00 -0400

    adding some id information

--- models/Instruction.js
+++ models/Instruction.js
@@ -5,6 +5,36 @@ const sequelize = require('../config/connection');
 
 class Instruction extends Model {}
 
-Instruction.init();
+Instruction.init(
+    {
+        //Every instruction is an object with a primary id.
+        id: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+          primaryKey: true,
+          autoIncrement: true,
+        },
+        //The name of the instruction is a string.
+        name: {
+          type: DataTypes.STRING,
+          allowNull: false,
+        },
+        // This references a recipe object stored in the database.
+        recipe_id: {
+            type: DataTypes.INTEGER,
+            references: {
+              // References the foreign table where the recipe_id is found.
+              model: 'recipe',
+              key: 'id',
+            },
+        },
+    
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'instruction',
+      }
+);
   
-  module.exports = Ingredient;
+  module.exports = Instruction;
