3f70ff224edbe728aec258c9379bf4493cc65b26
BinaryBitBytes <94703967+BinaryBitBytes@users.noreply.github.com>
2022-04-09 12:55:14 -0400

    updated the index.js with the attributes and ID's

--- models/index.js
+++ models/index.js
@@ -5,16 +5,30 @@ const Ingredient = require('./Ingredient');
 const IngredientAttribute = require('./IngridientAttribute');
 const Instruction = require('./Instruction');
 const SauceRecipe = require('./sauceRecipeModel');
-// const BaseIngredientAttribute = require('./BaseIngredientAttribute)
+// const BaseIngredientAttribute = require('./BaseIngredientAttribute);
 
 //Attribute belongsToMany Ingredients through IngredientAttribute
-
+Attribute.belongsToMany(Ingredient, {
+    foreignKey: 'Ingredient_id',
+    onDelete: 'CASCADE'
+});
 //Attribute belongsToMany BaseIngredients through BaseIngredientAttribute
-
+/*//!
+Attribute.belongsToMany(BaseIngredient, {
+    foreignKey: '',
+    onDelete: 'CASCADE'
+});
+*/
 //SauceRecipe hasMany Instructions
-
+SauceRecipe.hasMany(Instruction, {
+    foreignKey: 'Instruction_id',
+    onDelete: 'CASCADE'
+});
 //Instruction belongsTo SauceRecipe
-
+Instruction.belongsTo(SauceRecipe, {
+    foreignKey: 'SauceRecipe_id',
+    onDelete: 'CASCADE'
+});
 
 
 
