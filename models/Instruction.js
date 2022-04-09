374ae2ffa2287bef6a12e374963b81d2c6ca86f3
unknown <pearsenation@gmail.com>
2022-04-09 12:48:09 -0400

    adding some association

--- models/Instruction.js
+++ models/Instruction.js
@@ -6,47 +6,48 @@ const sequelize = require('../config/connection');
 class Instruction extends Model {}
 
 Instruction.init(
-    {
-        // Every instruction is an object with a primary id.
-        id: {
-          type: DataTypes.INTEGER,
-          allowNull: false,
-          primaryKey: true,
-          autoIncrement: true,
+  {
+    // Every instruction is an object with a primary id.
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+      // The name of the instruction is a string.
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    action: {
+        type: Dataypes.STRING,
+        allowNull: false,
+    },
+    // This references an ingredient id.
+    ingredient_id: {
+        type: DataTypes.INTEGER,
+        references: {
+            // References the foreign table where the ingredient_id is found.
+            model: 'Ingredient',
+            key: 'id',
         },
-        // The name of the instruction is a string.
-        name: {
-          type: DataTypes.STRING,
-          allowNull: false,
-        },
-        action: {
-            type: Dataypes.STRING,
-            allowNull: false,
-        },
-        // This references an ingredient id.
-        ingredient_id: {
-            type: DataTypes.INTEGER,
-            references: {
-                // References the foreign table where the ingredient_id is found.
-                model: 'ingredient',
-                key: 'id',
-            },
-        },
-        // This references and ingredient time used for cooking instructions.
-        //ingredient_time: {
-        //   type: DataTypes.INTEGER,
-        //    references: {
-        //        model: 'ingredient',
-        //        key: 'time',
-        //    },
-        //},
-    
-        sequelize,
-        timestamps: false,
-        freezeTableName: true,
-        underscored: true,
-        modelName: 'instruction',
-      }
+    },
+    // This references and ingredient time used for cooking instructions.
+    //ingredient_time: {
+    //   type: DataTypes.INTEGER,
+    //    references: {
+    //        model: 'ingredient',
+    //        key: 'time',
+    //    },
+    //},
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'Instruction',
+  }
 );
   
   module.exports = Instruction;
