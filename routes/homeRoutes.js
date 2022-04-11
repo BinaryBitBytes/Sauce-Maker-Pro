01bde454d10e0baf2bf1120da2d20e938d25e7e2
Cody S <cody.scoles@gmail.com>
2022-04-11 13:14:49 -0400

    Add routes directory

---
+++ routes/homeRoutes.js
@@ -0,0 +1,29 @@
+const router = require('express').Router();
+const { SauceRecipe, Ingredient } = require('../models');
+
+
+router.get('/', async (req, res) => {
+  try {
+    // Get all projects and JOIN with user data
+    const recipeData = await SauceRecipe.findAll({
+      include: [
+        {
+          model: Ingredient,
+          attributes: ['name'],
+        },
+      ],
+    });
+
+    // Serialize data so the template can read it
+    const recipes = recipeData.map((recipe) => recipe.get({ plain: true }));
+
+    // Pass serialized data and session flag into template
+    res.render('all', { 
+      recipes
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
