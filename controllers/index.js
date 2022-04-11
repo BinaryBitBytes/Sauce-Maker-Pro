c9958fab918a20fc2782865c7a044a5ae389fe60
BinaryBitBytes <94703967+BinaryBitBytes@users.noreply.github.com>
2022-04-11 18:55:22 -0400

    updated the index.js under controllers

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,10 @@
 const router = require('express').Router();
-
+//const apiRoutes = require('./api');
+const homeRoutes = require('./homeRoutes');
 const apiRoutes = require('./api');
 
+//router.use('/api', apiRoutes);
+router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
 
 module.exports = router;
