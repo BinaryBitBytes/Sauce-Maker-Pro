7cce5cf11a66bbb2ee399accd8af55d9973745e5
BinaryBitBytes <94703967+BinaryBitBytes@users.noreply.github.com>
2022-04-13 18:18:17 -0400

    added some details to recipe.handlebars

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -13,7 +13,8 @@ router.get('/', async (req, res) => {
     const recipes = recipeData.map((recipe) => recipe.get({ plain: true }));
 
     // Pass serialized data and session flag into template
-    res.render('all', { 
+    res.render('main', { //sends recipes that are requested into sauce-recipes.handlebars that way handlebars can grab all the json and we can use html and css
+      //we will need a recipe.handlebars view so we can have a res.render recipe so we can have a get route and post a html document instead of json
       recipes
     });
   } catch (err) {
