cea8c7d4c8fa30ecb97951a50bb1b201660bb0ac
Cody S <cody.scoles@gmail.com>
2022-04-13 21:01:09 -0400

    Some worthwhile changes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,16 +1,17 @@
 const router = require('express').Router();
-const { SauceRecipe, Ingredient, Attribute, IngredientAttribute } = require('../models');
+const { SauceRecipe, Ingredient, BaseIngredient } = require('../models');
 
 
 router.get('/', async (req, res) => {
   try {
     // Get all projects and JOIN with user data
     const recipeData = await SauceRecipe.findAll({
-     
+      include: [{model: BaseIngredient}, { model: Ingredient }],
     });
 
     // Serialize data so the template can read it
     const recipes = recipeData.map((recipe) => recipe.get({ plain: true }));
+    console.log(recipes);
 
     // Pass serialized data and session flag into template
     res.render('all', { 
