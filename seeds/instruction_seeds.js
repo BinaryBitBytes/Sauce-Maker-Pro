334b9aab60cd2c769e3933468cd7094c12e1c962
Cody S <cody.scoles@gmail.com>
2022-04-16 02:03:38 -0400

    Add instruction seed for each seed ingredient

--- seeds/instruction_seeds.js
+++ seeds/instruction_seeds.js
@@ -14,7 +14,35 @@ const instructionseedData = [
         "id": 2,
         "name": "Cook",
         "action": "Cook",
-        "ingredient_id": 1
+        "ingredient_id": 3
+        //^^ Action will be expanded to a full string to list out instructions to the user ^^
+    },
+    {
+        "id": 3,
+        "name": "Mush",
+        "action": "Mush grape jelly into sauce",
+        "ingredient_id": 8
+        //^^ Action will be expanded to a full string to list out instructions to the user ^^
+    },
+    {
+        "id": 4,
+        "name": "Cook",
+        "action": "Cook",
+        "ingredient_id": 2
+        //^^ Action will be expanded to a full string to list out instructions to the user ^^
+    },
+    {
+        "id": 5,
+        "name": "Squirt",
+        "action": "Squirt siracha onto sauce",
+        "ingredient_id": 4
+        //^^ Action will be expanded to a full string to list out instructions to the user ^^
+    },
+    {
+        "id": 3,
+        "name": "Blend",
+        "action": "Blend the chicken stock into the mix",
+        "ingredient_id": 20
         //^^ Action will be expanded to a full string to list out instructions to the user ^^
     }
 ]
